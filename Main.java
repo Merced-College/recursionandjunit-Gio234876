624a8356bde93eb876c2ae63b5a461c4f2f5efc7
Gio234876 <161302424+Gio234876@users.noreply.github.com>
2025-07-17 23:10:02 +0000

    #Get the foundation set for the initial Junit Test

---
+++ Main.java
@@ -0,0 +1,18 @@
+//Jose Giovanni Tapia Lopez
+//07/17/2025
+//Recursion and Junit Test assignment
+
+//make sure it has java standards
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD");
+    }
+
+
+    public static int count8(int number){
+        return 3;
+    }
+}
+
+ 
---
+++ RecursiveTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+
+public class RecursiveTest {
+    
+    @Test
+    public void test1(){
+        assertEquals(Main.count8(88188), 3);
+    }
+
+    @Test
+    public void test2(){
+        // assertEquals(Main.count8(88188), 3);
+    }
+
+    @Test
+    public void test3(){
+        // assertEquals(Main.count8(88188), 3);
+    }
+
+    @Test
+    public void test4(){
+        // assertEquals(Main.count8(88188), 3);
+    }
+
+    @Test
+    public void test5(){
+        // assertEquals(Main.count8(88188), 3);
+    }
+}
+
+//write method in main 
